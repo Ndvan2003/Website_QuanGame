612a7970acf0491a6223f45b7719f8c33f98ef02
Nguyễn Đình Văn <vann92044@gmail.com>
2025-06-12 00:19:09 +0700

    Update

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const uri = "mongodb://localhost:27017"; // Nếu dùng MongoDB local
+const uri = "mongodb://localhost:27017"; 
 const client = new MongoClient(uri);
 const dbName = "QLQG";
 
@@ -16,25 +16,25 @@ async function startServer() {
     await client.connect();
     const db = client.db(dbName);
     const stores = db.collection("gamestores");
-
+// lấy danh sách quán game
     app.get("/api/gamestores", async (req, res) => {
       const result = await stores.find({}).toArray();
       res.json(result);
     });
-
+//Thêm
     app.post("/api/gamestores", async (req, res) => {
       const data = req.body;
       const result = await stores.insertOne(data);
       res.status(201).json(result);
     });
-
+//sửa
     app.put("/api/gamestores/:id", async (req, res) => {
       const id = req.params.id;
       const data = req.body;
       const result = await stores.updateOne({ _id: new ObjectId(id) }, { $set: data });
       res.json(result);
     });
-
+//xóa
     app.delete("/api/gamestores/:id", async (req, res) => {
       const id = req.params.id;
       const result = await stores.deleteOne({ _id: new ObjectId(id) });
